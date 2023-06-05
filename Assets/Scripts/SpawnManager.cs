f85fbf2fadde8b87bc7f85bfbcbe3d238262075e
IAmMrLuck <114543523+IAmMrLuck@users.noreply.github.com>
2023-06-06 09:53:15 +1000

    fixed the spawn mechanic, now they only spawn outside the screen, but only just outside

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -19,17 +19,59 @@ namespace ConaLuk.Honey
 
         public void SpawnBeetle()
         {
+            GameObject newBeetle = Instantiate(beetleEnemy);
+            Vector3 screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
 
-            RandomSpawnPosition();
-            GameObject NewBeetle = Instantiate(beetleEnemy);
-            NewBeetle.transform.position = beetleSpawnPosition;
+            float xPos, yPos;
 
+            // Randomly determine which side of the screen to spawn the beetle
+            int side = Random.Range(0, 4);
+
+            // Randomly select position along the chosen side
+            if (side == 0) // Top side
+            {
+                xPos = Random.Range(-screenBounds.x, screenBounds.x);
+                yPos = screenBounds.y;
+            }
+            else if (side == 1) // Right side
+            {
+                xPos = screenBounds.x;
+                yPos = Random.Range(-screenBounds.y, screenBounds.y);
+            }
+            else if (side == 2) // Bottom side
+            {
+                xPos = Random.Range(-screenBounds.x, screenBounds.x);
+                yPos = -screenBounds.y;
+            }
+            else // Left side
+            {
+                xPos = -screenBounds.x;
+                yPos = Random.Range(-screenBounds.y, screenBounds.y);
+            }
+
+            // Randomly offset the position along the chosen side
+            float offset = Random.Range(1f, 2f);
+            Vector3 spawnPosition = new Vector3(xPos, yPos, 0) * offset;
+            newBeetle.transform.position = spawnPosition;
         }
 
-        public void RandomSpawnPosition()
-        {
-            beetleSpawnPosition = Random.insideUnitCircle * Random.Range(12, 12);
-        }
+        //public void SpawnBeetle()
+        //{
+
+        //    GameObject NewBeetle = Instantiate(beetleEnemy);
+        //    Vector2 screenBounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+
+        //    float xPos = Random.Range(screenBounds.x + 1, screenBounds.x + 2);
+        //    float yPos = Random.Range(screenBounds.y + 1, screenBounds.y + 2);
+        //    NewBeetle.transform.position = new Vector2(xPos, yPos);
+        //}
+
+        //public void RandomSpawnPosition()
+        //{
+
+
+        //    beetleSpawnPosition = Random.insideUnitCircle * Random.Range(11, 12);
+        //}
 
 
     }
